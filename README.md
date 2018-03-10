6623e4503b84319a486c33598cae0e516a98ebcb
redpencil <info@redpencil.io>
2018-03-10 21:26:48 +0100

    Initial open-source commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Demo editor
+
+Demo RDFa editor on top of the Besluit and Mandaat model and application profile as defined on:
+* http://data.vlaanderen.be/ns/besluit
+* http://data.vlaanderen.be/doc/applicatieprofiel/besluit-publicatie
+* http://data.vlaanderen.be/ns/mandaat
+* http://data.vlaanderen.be/doc/applicatieprofiel/mandatendatabank
+
+## Running the application
+```
+docker-compose up
+```
+
+The stack is built starting from [mu-project](https://github.com/mu-semtech/mu-project).
+
+OpenAPI documentation can be generated using [cl-resources-openapi-generator](https://github.com/mu-semtech/cl-resources-openapi-generator).
---
+++ config/resources/domain.lisp
@@ -0,0 +1,55 @@
+(in-package :mu-cl-resources)
+
+(defparameter *cache-count-queries* nil)
+(defparameter *include-count-in-paginated-responses* t
+  "when non-nil, all paginated listings will contain the number
+   of responses in the result object's meta.")
+
+(read-domain-file "master-besluit-domain.lisp")
+(read-domain-file "slave-mandaat-domain.lisp")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; TEMPLATES ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define-resource template ()
+  :class (s-prefix "ext:Template")
+  :properties `((:title :string ,(s-prefix "dct:title"))
+                (:matches :string-set ,(s-prefix "ext:templateMatches"))
+                (:body :string ,(s-prefix "ext:templateContent"))
+                (:contexts :uri-set ,(s-prefix "ext:activeInContext"))
+                (:disabled-in-contexts :uri-set ,(s-prefix "ext:disabledInContext")))
+  :resource-base (s-url "http://lblod.info/templates/")
+  :features `(no-pagination-defaults)
+  :on-path "templates")
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; DOCUMENTS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define-resource editor-document ()
+  :class (s-prefix "ext:EditorDocument")
+  :properties `((:title :string ,(s-prefix "dct:title"))
+                (:content :string ,(s-prefix "ext:editorDocumentContent"))
+                (:context :string ,(s-prefix "ext:editorDocumentContext"))
+                (:created-on :datetime ,(s-prefix "pav:createdOn"))
+                (:starred :boolean ,(s-prefix "tmp:starred"))
+                (:origin :string ,(s-prefix "pav:providedBy"))) ;;de gemeente Niel
+
+  :has-one `((editor-document-status :via ,(s-prefix "ext:editorDocumentStatus")
+                                     :as "status")
+             (editor-document        :via ,(s-prefix "pav:previousVersion")
+                                     :as "previous-version")
+             (editor-document :via ,(s-prefix "pav:previousVersion")
+                              :inverse t
+                              :as "next-version")
+             (zitting :via ,(s-prefix "besluit:heeftNotulen")
+                              :inverse t
+                              :as "zitting"))
+  :resource-base (s-url "http://lblod.info/editor-documents/")
+  :features `(no-pagination-defaults)
+  :on-path "editor-documents")
+
+(define-resource editor-document-status ()
+  :class (s-prefix "ext:EditorDocumentStatus")
+  :properties `((:name :string ,(s-prefix "ext:EditorDocumentStatusName")))
+  :resource-base (s-url "http://lblod.info/editor-document-statuses/")
+  :features `(no-pagination-defaults)
+  :on-path "editor-document-statuses")
