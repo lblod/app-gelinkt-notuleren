741545cbea846e68a820546ba1bd259f64b0e9b7
Niels Vandekeybus <progster@gmail.com>
2018-04-09 09:59:27 +0200

    updated dispatcher rules

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -25,147 +25,126 @@ defmodule Dispatcher do
   match "/agendas/*path" do
     Proxy.forward conn, path, "http://resource/agendas/"
   end
-
   match "/agendapunten/*path" do
     Proxy.forward conn, path, "http://resource/agendapunten/"
   end
-
   match "/artikels/*path" do
     Proxy.forward conn, path, "http://resource/artikels/"
   end
-
   match "/behandelingen-van-agendapunten/*path" do
     Proxy.forward conn, path, "http://resource/behandelingen-van-agendapunten/"
   end
-
   match "/besluiten/*path" do
     Proxy.forward conn, path, "http://resource/besluiten/"
   end
-
   match "/bestuurseenheden/*path" do
     Proxy.forward conn, path, "http://resource/bestuurseenheden/"
   end
-
-  match "/bestuurseenheid-classificatie-codes/*path" do
-    Proxy.forward conn, path, "http://resource/bestuurseenheid-classificatie-codes/"
-  end
-
-  match "/bestuursorganen/*path" do
-    Proxy.forward conn, path, "http://resource/bestuursorganen/"
-  end
-
-  match "/bestuursorgaan-classificatie-codes/*path" do
-    Proxy.forward conn, path, "http://resource/bestuursorgaan-classificatie-codes/"
-  end
-
-  match "/rechtsgronden-besluit/*path" do
-    Proxy.forward conn, path, "http://resource/rechtsgronden-besluit/"
-  end
-
-  match "/rechtsgronden-artikel/*path" do
-    Proxy.forward conn, path, "http://resource/rechtsgronden-artikel/"
-  end
-
-  match "/stemmingen/*path" do
-    Proxy.forward conn, path, "http://resource/stemmingen/"
-  end
-
-  match "/zittingen/*path" do
-    Proxy.forward conn, path, "http://resource/zittingen/"
-  end
-
-  match "/entiteiten/*path" do
-    Proxy.forward conn, path, "http://resource/entiteiten/"
-  end
-
-  match "/fracties/*path" do
-    Proxy.forward conn, path, "http://resource/fracties/"
-  end
-
-  match "/geboortes/*path" do
-    Proxy.forward conn, path, "http://resource/geboortes/"
-  end
-
-  match "/lijsttypes/*path" do
-    Proxy.forward conn, path, "http://resource/lijsttypes/"
-  end
-
-  match "/kandidatenlijsten/*path" do
-    Proxy.forward conn, path, "http://resource/kandidatenlijsten/"
-  end
-
-  match "/lidmaatschappen/*path" do
-    Proxy.forward conn, path, "http://resource/lidmaatschappen/"
-  end
-
-  match "/mandaten/*path" do
-    Proxy.forward conn, path, "http://resource/mandaten/"
-  end
-
-  match "/bestuursfunctie-codes/*path" do
-    Proxy.forward conn, path, "http://resource/bestuursfunctie-codes/"
-  end
-
-  match "/mandatarissen/*path" do
-    Proxy.forward conn, path, "http://resource/mandatarissen/"
-  end
-
-  match "/mandataris-status-codes/*path" do
-    Proxy.forward conn, path, "http://resource/mandataris-status-codes/"
-  end
-
-  match "/beleidsdomein-codes/*path" do
-    Proxy.forward conn, path, "http://resource/beleidsdomein-codes/"
-  end
-
-  match "/personen/*path" do
-    Proxy.forward conn, path, "http://resource/personen/"
-  end
-
-  match "/geslacht-codes/*path" do
-    Proxy.forward conn, path, "http://resource/geslacht-codes/"
-  end
-
-  match "/identificatoren/*path" do
-    Proxy.forward conn, path, "http://resource/identificatoren/"
-  end
-
-  match "/rechtsgronden-aanstelling/*path" do
-    Proxy.forward conn, path, "http://resource/rechtsgronden-aanstelling/"
-  end
-
-  match "/rechtsgronden-beeindiging/*path" do
-    Proxy.forward conn, path, "http://resource/rechtsgronden-beeindiging/"
-  end
-
-  match "/rechtstreekse-verkiezingen/*path" do
-    Proxy.forward conn, path, "http://resource/rechtstreekse-verkiezingen/"
-  end
-
-  match "/tijdsgebonden-entiteiten/*path" do
-    Proxy.forward conn, path, "http://resource/tijdsgebonden-entiteiten/"
-  end
-
-  match "/tijdsintervallen/*path" do
-    Proxy.forward conn, path, "http://resource/tijdsintervallen/"
-  end
-
-  match "/verkiezingsresultaten/*path" do
-    Proxy.forward conn, path, "http://resource/verkiezingsresultaten/"
-  end
-
-  match "/verkiezingsresultaat-gevolg-codes/*path" do
-    Proxy.forward conn, path, "http://resource/verkiezingsresultaat-gevolg-codes/"
-  end
-
   match "/werkingsgebieden/*path" do
     Proxy.forward conn, path, "http://resource/werkingsgebieden/"
   end
-
+  match "/bestuurseenheid-classificatie-codes/*path" do
+    Proxy.forward conn, path, "http://resource/bestuurseenheid-classificatie-codes/"
+  end
+  match "/bestuursorganen/*path" do
+    Proxy.forward conn, path, "http://resource/bestuursorganen/"
+  end
+  match "/bestuursorgaan-classificatie-codes/*path" do
+    Proxy.forward conn, path, "http://resource/bestuursorgaan-classificatie-codes/"
+  end
+  match "/rechtsgronden-besluit/*path" do
+    Proxy.forward conn, path, "http://resource/rechtsgronden-besluit/"
+  end
+  match "/rechtsgronden-artikel/*path" do
+    Proxy.forward conn, path, "http://resource/rechtsgronden-artikel/"
+  end
+  match "/stemmingen/*path" do
+    Proxy.forward conn, path, "http://resource/stemmingen/"
+  end
+  match "/zittingen/*path" do
+    Proxy.forward conn, path, "http://resource/zittingen/"
+  end
+  match "/entiteiten/*path" do
+    Proxy.forward conn, path, "http://resource/entiteiten/"
+  end
+  match "/fracties/*path" do
+    Proxy.forward conn, path, "http://resource/fracties/"
+  end
+  match "/geboortes/*path" do
+    Proxy.forward conn, path, "http://resource/geboortes/"
+  end
+  match "/lijsttypes/*path" do
+    Proxy.forward conn, path, "http://resource/lijsttypes/"
+  end
+  match "/kandidatenlijsten/*path" do
+    Proxy.forward conn, path, "http://resource/kandidatenlijsten/"
+  end
+  match "/lidmaatschappen/*path" do
+    Proxy.forward conn, path, "http://resource/lidmaatschappen/"
+  end
+  match "/mandaten/*path" do
+    Proxy.forward conn, path, "http://resource/mandaten/"
+  end
+  match "/bestuursfunctie-codes/*path" do
+    Proxy.forward conn, path, "http://resource/bestuursfunctie-codes/"
+  end
+  match "/mandatarissen/*path" do
+    Proxy.forward conn, path, "http://resource/mandatarissen/"
+  end
+  match "/mandataris-status-codes/*path" do
+    Proxy.forward conn, path, "http://resource/mandataris-status-codes/"
+  end
+  match "/beleidsdomein-codes/*path" do
+    Proxy.forward conn, path, "http://resource/beleidsdomein-codes/"
+  end
+  match "/personen/*path" do
+    Proxy.forward conn, path, "http://resource/personen/"
+  end
+  match "/geslacht-codes/*path" do
+    Proxy.forward conn, path, "http://resource/geslacht-codes/"
+  end
+  match "/identificatoren/*path" do
+    Proxy.forward conn, path, "http://resource/identificatoren/"
+  end
+  match "/rechtsgronden-aanstelling/*path" do
+    Proxy.forward conn, path, "http://resource/rechtsgronden-aanstelling/"
+  end
+  match "/rechtsgronden-beeindiging/*path" do
+    Proxy.forward conn, path, "http://resource/rechtsgronden-beeindiging/"
+  end
+  match "/rechtstreekse-verkiezingen/*path" do
+    Proxy.forward conn, path, "http://resource/rechtstreekse-verkiezingen/"
+  end
+  match "/tijdsgebonden-entiteiten/*path" do
+    Proxy.forward conn, path, "http://resource/tijdsgebonden-entiteiten/"
+  end
+  match "/tijdsintervallen/*path" do
+    Proxy.forward conn, path, "http://resource/tijdsintervallen/"
+  end
+  match "/verkiezingsresultaten/*path" do
+    Proxy.forward conn, path, "http://resource/verkiezingsresultaten/"
+  end
+  match "/verkiezingsresultaat-gevolg-codes/*path" do
+    Proxy.forward conn, path, "http://resource/verkiezingsresultaat-gevolg-codes/"
+  end
+  match "/vestigingen/*path" do
+    Proxy.forward conn, path, "http://resource/vestigingen/"
+  end
+  match "/contact-punten/*path" do
+    Proxy.forward conn, path, "http://resource/contact-punten/"
+  end
+  match "/posities/*path" do
+    Proxy.forward conn, path, "http://resource/posities/"
+  end
+  match "/rollen/*path" do
+    Proxy.forward conn, path, "http://resource/rollen/"
+  end
+  match "/organisaties/*path" do
+    Proxy.forward conn, path, "http://resource/organisaties/"
+  end
   match "/templates/*path" do
     Proxy.forward conn, path, "http://resource/templates/"
   end
-
   match "/editor-documents/*path" do
     Proxy.forward conn, path, "http://resource/editor-documents/"
   end
