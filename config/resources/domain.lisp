38cac6a311a770258ea7c94e9a7dfc0e46671679
Erika Pauwels <erika.pauwels@gmail.com>
2018-11-19 19:17:33 +0100

    Update authorization configuration and migrations

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -1,9 +1,13 @@
 (in-package :mu-cl-resources)
 
 (defparameter *cache-count-queries* nil)
+(defparameter *supply-cache-headers-p* t
+  "when non-nil, cache headers are supplied.  this works together with mu-cache.")
+(setf *cache-model-properties-p* t)
 (defparameter *include-count-in-paginated-responses* t
   "when non-nil, all paginated listings will contain the number
    of responses in the result object's meta.")
+(defparameter *max-group-sorted-properties* nil)
 
 (read-domain-file "slave-besluit-domain.lisp")
 (read-domain-file "slave-mandaat-domain.lisp")
