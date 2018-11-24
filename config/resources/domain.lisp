49a887c87a37f27f71dda730d8b6c399aabad162
Erika Pauwels <erika.pauwels@gmail.com>
2018-11-24 23:14:04 +0100

    Disable internal mu-cl-resources cache
    Internal caching doesn't cooperate nicely with the notulen-importer service

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -3,7 +3,7 @@
 (defparameter *cache-count-queries* nil)
 (defparameter *supply-cache-headers-p* t
   "when non-nil, cache headers are supplied.  this works together with mu-cache.")
-(setf *cache-model-properties-p* t)
+;;(setf *cache-model-properties-p* t)
 (defparameter *include-count-in-paginated-responses* t
   "when non-nil, all paginated listings will contain the number
    of responses in the result object's meta.")
