abf91947e6576d43d454779d3b45fe27076b3e40
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-01-07 16:33:50 +0100

    added delta rules for publicatie-melding

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -12,5 +12,20 @@ export default [
       gracePeriod: 1000,
       ignoreFromSelf: true
     }
+  },
+  {
+    match: {
+      // form of element is {subject,predicate,object}
+      predicate: { type: "uri", value: "http://mu.semte.ch/vocabularies/ext/besluit-publicatie-publish-service/status" },
+      object: { type: "uri", value: "http://mu.semte.ch/vocabularies/ext/besluit-publicatie-publish-service/status/success" }
+    },
+    callback: {
+      url: "http://publicatie-melding/submit-publication", method: "POST"
+    },
+    options: {
+      resourceFormat: "v0.0.1",
+      gracePeriod: 1000,
+      ignoreFromSelf: true
+    }
   }
 ]
