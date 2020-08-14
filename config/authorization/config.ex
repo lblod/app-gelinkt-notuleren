d1da564758e2b504851e8edb210b3a99724fe25c
Oscar <lagartoverde97@gmail.com>
2020-08-14 12:47:17 +0200

    added resources for the labels to mu-auth

--- config/authorization/config.ex
+++ config/authorization/config.ex
@@ -80,7 +80,12 @@ defmodule Acl.UserGroups.Config do
                         "http://data.lblod.info/vocabularies/mobiliteit/MaatregelConcept",
                         "https://data.vlaanderen.be/ns/mobiliteit#Verkeersbordcategorie",
                         "https://data.vlaanderen.be/ns/mobiliteit#Verkeersbordconcept",
-                        "http://data.lblod.info/vocabularies/mobiliteit/VerkeersbordconceptStatusCode"
+                        "http://data.lblod.info/vocabularies/mobiliteit/VerkeersbordconceptStatusCode",
+                        "http://www.w3.org/2000/01/rdf-schema#Class",
+                        "http://www.w3.org/2002/07/owl#Class",
+                        "http://www.w3.org/1999/02/22-rdf-syntax-ns#Property",
+                        "http://www.w3.org/2002/07/owl#DatatypeProperty",
+                        "http://www.w3.org/2002/07/owl#ObjectProperty",
                       ]
                     } },
                   %GraphSpec{
