3d6ff34a4e6c7d0e20b30b82bc3026d80723a388
Niels V <nvdk@users.noreply.github.com>
2020-11-25 10:49:03 +0100

    add missing prefix

--- config/resources/repository.lisp
+++ config/resources/repository.lisp
@@ -33,3 +33,4 @@
 (add-prefix "locn" "http://www.w3.org/ns/locn#")
 (add-prefix "adres" "https://data.vlaanderen.be/ns/adres#")
 (add-prefix "lblodlg" "http://data.lblod.info/vocabularies/leidinggevenden/")
+(add-prefix "bibo" "http://purl.org/ontology/bibo/")
