57966b2f1be34bfb47e3210d2a02dc99196bf93d
Elena Poelman <elena.poelman@redpencil.io>
2023-05-10 10:32:56 +0200

    include commented versions of old, overwritten json-api models

--- config/resources/models/functionaris-status-code.lisp
+++ config/resources/models/functionaris-status-code.lisp
@@ -5,3 +5,13 @@
   :resource-base (s-url "http://data.vlaanderen.be/id/concept/functionarisStatusCode/")
   :features '(include-uri)
   :on-path "functionaris-status-codes")
+
+;; Old (overwritten) version of functionaris-status-code
+;;
+;; (define-resource functionaris-status-code ()
+;;   ;;TODO: publish this somewhere
+;;   :class (s-prefix "lblod:FunctionarisStatusCode")
+;;   :properties `((:label :string ,(s-prefix "skos:prefLabel")))
+;;   :resource-base (s-url "http://data.vlaanderen.be/id/concept/functionarisStatusCode/")
+;;   :features '(include-uri)
+;;   :on-path "functionaris-status-codes")
--- config/resources/models/functionaris.lisp
+++ config/resources/models/functionaris.lisp
@@ -11,3 +11,24 @@
   :resource-base (s-url "http://data.lblod.info/id/functionarissen/")
   :features '(include-uri)
   :on-path "functionarissen")
+
+;; Old (overwritten) version of functionaris
+;;
+;; (define-resource functionaris ()
+  ;;Functionaris is considered subclass of org:Membership as of now. TODO: publish this
+;;   :class (s-prefix "lblod:Functionaris")
+;;   :has-one `((tijdsinterval :via ,(s-prefix "org:memberDuring")
+;;                             :as "lid-van-tot")
+;;              (persoon :via ,(s-prefix "org:member")
+;;                       :as "lid")
+;;              (rol :via ,(s-prefix "org:role")
+;;                   :as "rol")
+;;              (bestuurseenheid :via ,(s-prefix "org:organization")
+;;                               :as "is-lidmaatschap-bij")
+;;              (functionaris-status-code :via ,(s-prefix "lblod:functionarisStatusCode")
+;;                                        :as "status"))
+
+;;   :resource-base (s-url "http://data.lblod.info/id/functionaris/")
+;;   :features '(include-uri)
+;;   :on-path "functionarissen"
+;;   )
