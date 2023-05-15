87f1b362ca6ff5e0340c943fd844b787c782d8ad
Elena Poelman <elena.poelman@redpencil.io>
2023-05-15 11:11:17 +0200

    remove obsolete vestiging model

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -1142,16 +1142,6 @@
   :on-path "adressen"
 )
 
-;;TODO: is this model still in use/relevant?
-(define-resource vestiging ()
-  :class (s-prefix "org:Site")
-  :has-one `((contact-punt :via ,(s-prefix "schema:contactPoint")
-                           :as "vestigingsadres"))
-  :resource-base (s-url "http://data.lblod.info/id/vestiging/")
-  :features '(include-uri)
-  :on-path "vestigingen"
-)
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; PERSON-RELATED DATA MODELS ;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
