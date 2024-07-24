5fbf71f4e85aaf6756cc1c84bf458a5eb0e4e799
Oscar <lagartoverde97@gmail.com>
2024-07-24 11:14:07 +0200

    Consume from LDES Feed

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -35,4 +35,24 @@ export default [
       ignoreFromSelf: true,
     },
   },
+  {
+    match: {
+      graph: {
+        type: 'uri',
+        value: 'http://mu.semte.ch/graphs/lmb-mandaten-staging',
+      },
+    },
+    callback: {
+      url: 'http://lmb-upsert-ldes-entity/delta',
+      method: 'POST',
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true,
+      optOutMuScopeIds: [
+        'http://redpencil.data.gift/id/concept/muScope/deltas/initialSync',
+      ],
+    },
+  },
 ];
