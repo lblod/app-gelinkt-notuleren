feb3e7f57a30c3bd61fd35617a8012df9cd5548a
Ruben <ruben.de.facq@redpencil.io>
2024-09-17 14:19:40 +0200

    remove unneeded administrative-unit config

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -1244,19 +1244,6 @@
   :on-path "status-codes"
 )
 
-; Same as resource bestuurseenheid, but this is the config similar to loketportaal for hackathon
-(define-resource administrative-unit ()
-  :class (s-prefix "besluit:Bestuurseenheid")
-  :properties `((:name :string ,(s-prefix "skos:prefLabel"))
-                 (:classification :url ,(s-prefix "besluit:classificatie")))
-  :has-many `((submission :via ,(s-prefix "omgeving:Rechtshandeling.verantwoordelijke")
-                :as "submissions"
-                :inverse t))
-  :resource-base (s-url "http://data.lblod.info/id/bestuurseenheden/")
-  :features '(include-uri)
-  :on-path "administrative-units"
-)
-
 (define-resource organization ()
   :class (s-prefix "org:Organization")
   :properties `((:name :string ,(s-prefix "skos:prefLabel")))
