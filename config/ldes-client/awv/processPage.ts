da177e81292d3469a430cf11ed25027d29e04cb3
Oscar <lagartoverde97@gmail.com>
2025-12-16 17:17:04 +0100

    heftbrokene should move signalisatie ontwerp

--- config/ldes-client/awv/processPage.ts
+++ config/ldes-client/awv/processPage.ts
@@ -203,6 +203,87 @@ async function moveByType(urisWithType: { uri: string; type?: string }[]) {
   }
 }
 
+async function moveHeeftBetrokkene(uri: string) {
+  const graphQuery = `
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX relatie: <https://wegenenverkeer.data.vlaanderen.be/ns/implementatieelement#RelatieObject.>
+    PREFIX onderdeel: <https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#>
+    PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+    select distinct ?adminUnitUuid where {
+          GRAPH ${LDES_GRAPH} {
+            ${sparqlEscapeUri(uri)} a onderdeel:HeeftBetrokkene ;
+              relatie:bron ?signalisatieOntwerp;
+              relatie:doel ?ovoUri.
+            
+          }
+          GRAPH ${PUBLIC_GRAPH} {
+            ?adminUnit owl:sameAs ?ovoUri;
+              mu:uuid ?adminUnitUuid.
+          }
+      }`;
+  const queryResult = await querySudo(graphQuery);
+  const adminUnitUuid = queryResult.results.bindings[0]?.adminUnitUuid.value;
+  if (!adminUnitUuid) {
+    logger.error(`No admin unit found for ${uri}`);
+    return;
+  }
+  const graph = `http://mu.semte.ch/graphs/awv/ldes/${adminUnitUuid}`;
+  const moveQuery = `
+    DELETE {
+      GRAPH ${sparqlEscapeUri(graph)} {
+        ${sparqlEscapeUri(uri)} ?pOld ?oOld.
+      }
+    } WHERE {
+     OPTIONAL {
+        GRAPH ${sparqlEscapeUri(graph)} {
+          ${sparqlEscapeUri(uri)} ?pOld ?oOld.
+        }
+      }
+    };
+
+    INSERT {
+      GRAPH ${sparqlEscapeUri(graph)} {
+        ${sparqlEscapeUri(uri)} ?a ?b
+      }
+    } WHERE {
+      GRAPH ${LDES_GRAPH} {
+        ${sparqlEscapeUri(uri)} ?a ?b
+      }
+      
+    }
+  `;
+  await updateSudo(moveQuery, {}, sudoOptions);
+
+  const queryUrisToMove = `
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX relatie: <https://wegenenverkeer.data.vlaanderen.be/ns/implementatieelement#RelatieObject.>
+    PREFIX onderdeel: <https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#>
+    PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+    select distinct ?uriToMove where {
+     GRAPH ${LDES_GRAPH} {
+        ${sparqlEscapeUri(uri)} a onderdeel:HeeftBetrokkene ;
+              relatie:bron ?uriToMove.
+      }
+      FILTER NOT EXISTS {
+        GRAPH ${sparqlEscapeUri(graph)} {
+          ?uriToMove ?pred ?obj.
+        }
+      }
+    }
+  `;
+
+  const moveQueryResult = await querySudo(queryUrisToMove, {}, sudoOptions);
+  const urisToMove = moveQueryResult.results.bindings.map(
+    (binding) => binding.uriToMove.value
+  );
+  for (const uriToMove of urisToMove) {
+    await moveSignalisatieOntwerp(uriToMove);
+  }
+}
+
 async function moveSignalisatieOntwerp(uri: string) {
   const graphQuery = `
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
@@ -1310,60 +1391,6 @@ async function moveVerkeersbordopstelling(uri: string) {
           }
       }`;
   const queryResult = await querySudo(graphQuery, {}, sudoOptions);
-  const adminUnitUuid = queryResult.results.bindings[0]?.adminUnitUuid.value;
-  if (!adminUnitUuid) {
-    logger.error(`No admin unit found for ${uri}`);
-    return;
-  }
-  const graph = `http://mu.semte.ch/graphs/awv/ldes/${adminUnitUuid}`;
-  const moveQuery = `
-    DELETE {
-      GRAPH ${sparqlEscapeUri(graph)} {
-        ${sparqlEscapeUri(uri)} ?pOld ?oOld.
-      }
-    } WHERE {
-     OPTIONAL {
-        GRAPH ${sparqlEscapeUri(graph)} {
-          ${sparqlEscapeUri(uri)} ?pOld ?oOld.
-        }
-      }
-    };
-
-    INSERT {
-      GRAPH ${sparqlEscapeUri(graph)} {
-        ${sparqlEscapeUri(uri)} ?a ?b
-      }
-    } WHERE {
-      GRAPH ${LDES_GRAPH} {
-        ${sparqlEscapeUri(uri)} ?a ?b
-      }
-      
-    }
-  `;
-  await updateSudo(moveQuery, {}, sudoOptions);
-}
-
-async function moveHeeftBetrokkene(uri: string) {
-  const graphQuery = `
-    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX relatie: <https://wegenenverkeer.data.vlaanderen.be/ns/implementatieelement#RelatieObject.>
-    PREFIX onderdeel: <https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#>
-    PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
-    PREFIX owl: <http://www.w3.org/2002/07/owl#>
-    select distinct ?adminUnitUuid where {
-          GRAPH ${LDES_GRAPH} {
-            ${sparqlEscapeUri(uri)} a onderdeel:HeeftBetrokkene ;
-              relatie:bron ?signalisatieOntwerp;
-              relatie:doel ?ovoUri.
-            
-          }
-          GRAPH ${PUBLIC_GRAPH} {
-            ?adminUnit owl:sameAs ?ovoUri;
-              mu:uuid ?adminUnitUuid.
-          }
-      }`;
-  const queryResult = await querySudo(graphQuery);
   const adminUnitUuid = queryResult.results.bindings[0]?.adminUnitUuid.value;
   if (!adminUnitUuid) {
     logger.error(`No admin unit found for ${uri}`);
